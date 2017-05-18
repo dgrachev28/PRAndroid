2a1e451de932b793b6b79254e444ae079aa21633
dgrachev28 <dgrachev28@gmail.com>
2017-05-19 01:17:53 +0300

    Data binding for picture view activity

--- app/src/main/kotlin/com/company/prandroid/PictureViewActivity.kt
+++ app/src/main/kotlin/com/company/prandroid/PictureViewActivity.kt
@@ -1,25 +1,26 @@
 package com.company.prandroid
 
+import android.databinding.DataBindingUtil
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Base64
-import android.widget.ImageView
+import com.company.prandroid.databinding.ActivityPictureViewBinding
 import com.company.prandroid.dto.PictureDto
+import com.company.prandroid.viewmodel.MainViewModel.Companion.PICTURE_DTO
 
 class PictureViewActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPictureViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_picture_view)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_picture_view)
 
-        val pictureDto = intent.getSerializableExtra("pictureDto") as PictureDto
-        println(pictureDto.title)
-
+        val pictureDto = intent.getSerializableExtra(PICTURE_DTO) as PictureDto
         val decodedImage = Base64.decode(pictureDto.imageBase64, Base64.DEFAULT)
         val bmp = BitmapFactory.decodeByteArray(decodedImage, 0, decodedImage.size)
-        val imageView = findViewById(R.id.imageView) as ImageView
-        imageView.setImageBitmap(Bitmap.createBitmap(bmp))
+        binding.imageView.setImageBitmap(Bitmap.createBitmap(bmp))
     }
 }
